e68c31ef84b3adcf0523469ac05345ae24d3c37e
ShingHikaru <fiveh.5h@gmail.com>
2022-08-17 15:02:52 +0900

    Search bar layout adjustment
    Inserted an icon and fine-tuned the layout of the store name search bar on the home screen.

--- Screens/Home/SearchPlaceNameBar.js
+++ Screens/Home/SearchPlaceNameBar.js
@@ -1,28 +1,67 @@
-import {View, Text, TextInput, StyleSheet} from 'react-native';
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+} from 'react-native';
 import React from 'react';
 
 export default function SearchPlaceNameBar() {
-  let placeholderText = 'Food, groceries, drinks, etc.';
+  const placeholderText = 'Food, groceries, drinks, etc.';
   return (
-    <View>
-      <TextInput
-        style={styles.outLine}
-        placeholder={placeholderText}
-        placeholderTextColor="#6B6B6B"
-        selectionColor={'#4758'}
-        caretHidden={false}
-      />
-    </View>
+    <>
+      <View style={[styles.outLine]}>
+        {/* onPressには、TextInputと同じFunctionをcall */}
+        <TouchableOpacity onPress={() => {}}>
+          <Image
+            style={styles.tinyLogo}
+            source={require('../assets/Home/search.png')}
+          />
+        </TouchableOpacity>
+        <TextInput
+          style={styles.textinput}
+          // カーソルがタップされたら発火
+          onFocus={() => {}}
+          placeholder={placeholderText}
+          placeholderTextColor="#6B6B6B"
+          selectionColor={'#4758'}
+          caretHidden={false}
+          inlineImageLeft="../assets/Home/filter.png"
+        />
+        {/* onPressには、TextInputと同じFunctionをcall */}
+        <TouchableOpacity onPress={() => {}}>
+          {/* タッチの範囲拡張のため */}
+          <Image
+            style={styles.tinyLogo}
+            source={require('../assets/Home/filter.png')}
+          />
+        </TouchableOpacity>
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   outLine: {
-    padding: 20,
+    paddingTop: 8,
+    paddingBottom: 8,
+    paddingLeft: 16,
+    paddingRight: 16,
     alignSelf: 'center',
-    height: 36,
     width: '90%',
     backgroundColor: '#eee',
     borderRadius: 300,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  // 画面中央から、一定幅左に移動
+  textinput: {
+    right: '100%',
+  },
+  tinyLogo: {
+    width: 32,
+    height: 32,
   },
 });
